5c9cb1c96664cdb1eb0158d74ba002214adfb18b
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-02-08 12:17:59 +0545

    firebase client side setup

---
+++ firebaseSetup.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBLqVth_HGlI8jhuNJbvhIx6PawuDGZbvs",
+    authDomain: "h4cregistration.firebaseapp.com",
+    projectId: "h4cregistration",
+    storageBucket: "h4cregistration.appspot.com",
+    messagingSenderId: "1066241744146",
+    appId: "1:1066241744146:web:a2807290a78e4746c067cb",
+    measurementId: "G-XWP2H63WM0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
