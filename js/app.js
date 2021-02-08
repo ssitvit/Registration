61640d7ee129fd572a332491c4d47e4016fb7f74
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-02-08 21:54:00 +0545

    individual registration complete

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 const db = firebase.firestore();
+const form = document.querySelector("#form");
 
 const registerIndivisual = async(data) => {
   try{
     await db.collection('participants').doc(data.regNo).set({
       name:data.name,
       regNo: data.regNo,
+      email: data.email,
       phone: data.phone
     },{merge:true})
   } catch (e){
@@ -13,9 +15,17 @@ const registerIndivisual = async(data) => {
   
 }
 
-function gotoForm(){
-  location.href = "/form.html"
-}
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  var data = {};
+  data.name = form.querySelector("#name").value;
+  data.regNo = form.querySelector("#regno").value;
+  data.email = form.querySelector("#email").value;
+  data.phone = form.querySelector("#phone").value;
+  await registerIndivisual(data);
+  location.href = "/thanks.html"
+})
+
 
 // registerIndivisual({
 //   name:"Gunjan",
